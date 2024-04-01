44ca583766493845a28dd8ac7e8ad6cb1e2fd046
Kimjongmin <rlajm1203@naver.com>
2024-04-01 14:18:30 +0900

    feat: Health Contoller swagger 적용

--- eeos/src/main/java/com/blackcompany/eeos/health/HealthController.java
+++ eeos/src/main/java/com/blackcompany/eeos/health/HealthController.java
@@ -4,6 +4,7 @@ import com.blackcompany.eeos.common.presentation.respnose.ApiResponse;
 import com.blackcompany.eeos.common.presentation.respnose.ApiResponseBody.SuccessBody;
 import com.blackcompany.eeos.common.presentation.respnose.ApiResponseGenerator;
 import com.blackcompany.eeos.common.presentation.respnose.MessageCode;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class HealthController {
 
+	@Operation(summary = "헬스 체크를 한다.", description = "서버가 정상적으로 동작하는지 확인한다.")
 	@GetMapping("/health-check")
 	public ApiResponse<SuccessBody<Void>> checkHealth() {
 		return ApiResponseGenerator.success(HttpStatus.OK, MessageCode.GET);
