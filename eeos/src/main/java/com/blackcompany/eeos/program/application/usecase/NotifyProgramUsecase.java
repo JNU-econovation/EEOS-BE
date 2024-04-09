4759ec31089744f78f66142410a2b81c8d26e4fc
Kimjongmin <rlajm1203@naver.com>
2024-04-09 09:49:40 +0900

    update: 파일 경로 변경에 따른 수정

--- eeos/src/main/java/com/blackcompany/eeos/program/application/usecase/NotifyProgramUsecase.java
+++ eeos/src/main/java/com/blackcompany/eeos/program/application/usecase/NotifyProgramUsecase.java
@@ -1,8 +1,7 @@
 package com.blackcompany.eeos.program.application.usecase;
 
-import com.blackcompany.eeos.auth.presentation.support.Member;
 import com.blackcompany.eeos.program.application.dto.CommandProgramResponse;
-import com.blackcompany.eeos.program.infra.api.slack.chat.dto.ProgramSlackNotificationRequest;
+import com.blackcompany.eeos.program.application.dto.ProgramSlackNotificationRequest;
 
 public interface NotifyProgramUsecase {
 
