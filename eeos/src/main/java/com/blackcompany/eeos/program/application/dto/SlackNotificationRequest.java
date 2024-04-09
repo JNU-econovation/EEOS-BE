9223060eaee4d503a5f558b7d3742db0c5ef8428
Kimjongmin <rlajm1203@naver.com>
2024-04-09 09:46:56 +0900

    rename: 파일 경로 변경에 따른 파일명 변경

---
+++ eeos/src/main/java/com/blackcompany/eeos/program/application/dto/SlackNotificationRequest.java
@@ -0,0 +1,6 @@
+package com.blackcompany.eeos.program.application.dto;
+
+import com.blackcompany.eeos.common.support.dto.AbstractRequestDto;
+
+public interface SlackNotificationRequest extends AbstractRequestDto {
+}
