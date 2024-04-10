641e0476c22b7e84e90be4195803a9d85299f4e2
Kimjongmin <rlajm1203@naver.com>
2024-04-10 17:11:03 +0900

    update: @Configuration 추가 및 오타 수정

--- eeos/src/main/java/com/blackcompany/eeos/program/infra/api/slack/chat/model/BotTokens.java
+++ eeos/src/main/java/com/blackcompany/eeos/program/infra/api/slack/chat/model/BotTokens.java
@@ -2,15 +2,18 @@ package com.blackcompany.eeos.program.infra.api.slack.chat.model;
 
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @PropertySource("classpath:/env.properties")
 @Getter
-@Component
+@Configuration
 public class BotTokens {
-	@Value("${slack.bot.black-company.eeos-test")
+
+	@Value("${slack.bot.black-company.eeos}")
 	private String BLACK_COMPANY_EEOS_BOT;
 
-	private String ECONOVATION_EEOS_BOT = "";
+	@Value("${slack.bot.econovation.eeos}")
+	private String ECONOVATION_EEOS_BOT;
 }
--- eeos/src/main/java/com/blackcompany/eeos/program/infra/api/slack/chat/model/Channels.java
+++ eeos/src/main/java/com/blackcompany/eeos/program/infra/api/slack/chat/model/Channels.java
@@ -2,16 +2,17 @@ package com.blackcompany.eeos.program.infra.api.slack.chat.model;
 
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @PropertySource("classpath:/env.properties")
-@Component
+@Configuration
 @Getter
 public class Channels {
 	@Value("${slack.channel.econovation.notification}")
 	private String ECONOVATION_NOTIFICATION;
 
-	@Value("${slack.channel.black-company.eeos-test}")
+	@Value("${slack.channel.black-company.slack-message-test}")
 	private String BLACK_COMPANY_TEST;
 }
