fd81b89ee76916f00a8a1ae4af799cb74c5e53e2
Kimjongmin <rlajm1203@naver.com>
2024-04-10 17:08:25 +0900

    update: SlackChatApiClient post메소드 파라미터 변경

--- eeos/src/main/java/com/blackcompany/eeos/program/infra/api/slack/chat/client/SlackChatApiClient.java
+++ eeos/src/main/java/com/blackcompany/eeos/program/infra/api/slack/chat/client/SlackChatApiClient.java
@@ -2,8 +2,12 @@ package com.blackcompany.eeos.program.infra.api.slack.chat.client;
 
 import com.blackcompany.eeos.program.infra.api.slack.chat.dto.SlackChatPostMessageResponse;
 import com.blackcompany.eeos.program.infra.api.slack.chat.model.ChatPostModel;
+import com.sun.xml.bind.v2.runtime.output.Encoded;
 
 public interface SlackChatApiClient {
 
-	<T> SlackChatPostMessageResponse post(ChatPostModel model);
+	SlackChatPostMessageResponse post(final String channel,
+										  final String blocks,
+										  final String username,
+										  final String pretty);
 }
