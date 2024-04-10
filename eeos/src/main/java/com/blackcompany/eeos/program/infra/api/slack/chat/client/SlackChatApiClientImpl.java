13d7c7d68159b23eee92725ea8974c5e2dc3d8e1
Kimjongmin <rlajm1203@naver.com>
2024-04-10 17:08:57 +0900

    update: SlackChatApiClientImpl post메소드 파라미터 변경 및 Content-type 추가

--- eeos/src/main/java/com/blackcompany/eeos/program/infra/api/slack/chat/client/SlackChatApiClientImpl.java
+++ eeos/src/main/java/com/blackcompany/eeos/program/infra/api/slack/chat/client/SlackChatApiClientImpl.java
@@ -1,14 +1,21 @@
 package com.blackcompany.eeos.program.infra.api.slack.chat.client;
 
+import com.blackcompany.eeos.program.infra.api.slack.chat.config.FeignConfig;
 import com.blackcompany.eeos.program.infra.api.slack.chat.dto.SlackChatPostMessageResponse;
 import com.blackcompany.eeos.program.infra.api.slack.chat.model.ChatPostModel;
+import com.sun.xml.bind.v2.runtime.output.Encoded;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "SlackChatOpenFeign", url = "https://slack.com/api")
+@FeignClient(name = "SlackChatOpenFeign", url = "https://slack.com/api", configuration = FeignConfig.class)
 public interface SlackChatApiClientImpl extends SlackChatApiClient {
 
-	@PostMapping(path = "/chat.postMessage")
-	SlackChatPostMessageResponse post(@RequestBody ChatPostModel model);
+	@PostMapping(path = "/chat.postMessage", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+	SlackChatPostMessageResponse post(@RequestParam("channel") final String channel,
+									  @RequestParam("blocks") final String blocks,
+									  @RequestParam("username") final String username,
+									  @RequestParam("pretty") final String pretty);
 }
