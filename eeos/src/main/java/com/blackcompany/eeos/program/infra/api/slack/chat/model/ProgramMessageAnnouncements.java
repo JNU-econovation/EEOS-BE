bced653c38c38507f379a9506d250f9be420d65d
Bellmin <71818636+rlajm1203@users.noreply.github.com>
2024-04-12 05:30:54 +0900

    [UPDATE] 날짜 형식 및 멘트 일부 수정 (#57)

--- eeos/src/main/java/com/blackcompany/eeos/program/infra/api/slack/chat/model/ProgramMessageAnnouncements.java
+++ eeos/src/main/java/com/blackcompany/eeos/program/infra/api/slack/chat/model/ProgramMessageAnnouncements.java
@@ -6,9 +6,9 @@ import lombok.Getter;
 public enum ProgramMessageAnnouncements {
 	HEADER_TOP("@channel \n*[EEOS 행사 생성 알림]*"),
 	HEADER_MID("🎉EEOS 행사가 생성되었습니다!🎉"),
-	PROGRAM_NAME("1. 행사 제목\n"),
-	PROGRAM_DATE("2. 행사 일정\n"),
-	PROGRAM_URL("3. 행사 링크\n"),
+	PROGRAM_NAME("*① 행사 제목*\n"),
+	PROGRAM_DATE("*② 행사 일정*\n"),
+	PROGRAM_URL("*③ 행사 링크*\n"),
 	BOTTOM("행사에 관한 세부 내용과 출석 여부는 EEOS를 이용해주세요!");
 
 	private String announcement;
