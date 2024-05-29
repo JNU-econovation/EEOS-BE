27d4b6aa79b22333904e24f97138dec37270316b
KimJongMin <whdals011203@naver.com>
2024-05-30 00:06:43 +0900

    feat: Account Entity 작성

---
+++ eeos/src/main/java/com/blackcompany/eeos/auth/persistence/AccountEntity.java
@@ -0,0 +1,39 @@
+package com.blackcompany.eeos.auth.persistence;
+
+
+import com.blackcompany.eeos.common.persistence.BaseEntity;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@ToString
+@SuperBuilder(toBuilder = true)
+@Entity
+@Table(
+        name = AccountEntity.ENTITY_PREFIX,
+        indexes = {
+                @Index(name = "idx_member_id", columnList = "member_id")
+        })
+public class AccountEntity extends BaseEntity {
+
+    public static final String ENTITY_PREFIX = "account";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = ENTITY_PREFIX + "_id",nullable = false)
+    private Long id;
+
+    @Column(name = ENTITY_PREFIX + "member_id", nullable = false, unique = true)
+    private Long memberId;
+
+    @Column(name = ENTITY_PREFIX + "_login_id", nullable = false, unique = true)
+    private String loginId;
+
+    @Column(name = ENTITY_PREFIX + "_login_passwd", nullable = false)
+    private String passWd;
+
+}
