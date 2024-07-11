325e0334246d5bc08286348c6151f105d9bcf081
[CLOUD4] 김다애 <wlsemrdl1212@naver.com>
2024-07-11 21:54:55 +0900

    feat : 멤버 활동상태 수정, 삭제 로직 작성

--- eeos/src/main/java/com/blackcompany/eeos/member/application/service/CommandMemberService.java
+++ eeos/src/main/java/com/blackcompany/eeos/member/application/service/CommandMemberService.java
@@ -3,13 +3,16 @@ package com.blackcompany.eeos.member.application.service;
 import com.blackcompany.eeos.member.application.dto.ChangeActiveStatusRequest;
 import com.blackcompany.eeos.member.application.dto.CommandMemberResponse;
 import com.blackcompany.eeos.member.application.dto.converter.CommandMemberResponseConverter;
+import com.blackcompany.eeos.member.application.exception.DeniedMemberEditException;
 import com.blackcompany.eeos.member.application.exception.NotFoundMemberException;
 import com.blackcompany.eeos.member.application.model.MemberModel;
 import com.blackcompany.eeos.member.application.model.converter.MemberEntityConverter;
 import com.blackcompany.eeos.member.application.usecase.ChangeActiveStatusUsecase;
+import com.blackcompany.eeos.member.event.DeletedMemberEvent;
 import com.blackcompany.eeos.member.persistence.MemberEntity;
 import com.blackcompany.eeos.member.persistence.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,6 +22,7 @@ public class CommandMemberService implements ChangeActiveStatusUsecase {
 	private final MemberRepository memberRepository;
 	private final MemberEntityConverter memberConverter;
 	private final CommandMemberResponseConverter responseConverter;
+	private final ApplicationEventPublisher applicationEventPublisher;
 
 	@Transactional
 	@Override
@@ -36,8 +40,49 @@ public class CommandMemberService implements ChangeActiveStatusUsecase {
 				updatedMember.getName(), updatedMember.getActiveStatus().getStatus());
 	}
 
+	@Transactional
+	@Override
+	public CommandMemberResponse adminChangeStatus(
+			final Long adminMemberId, Long memberId, final ChangeActiveStatusRequest request) {
+		isAdmin(adminMemberId);
+		MemberModel model =
+				memberRepository
+						.findById(memberId)
+						.map(memberConverter::from)
+						.orElseThrow(NotFoundMemberException::new);
+
+		MemberEntity updateMember = updateActiveStatus(model, request.getActiveStatus());
+
+		return responseConverter.from(
+				updateMember.getName(), updateMember.getActiveStatus().getStatus());
+	}
+
 	private MemberEntity updateActiveStatus(final MemberModel model, final String status) {
 		MemberModel memberModel = model.updateActiveStatus(status);
 		return memberRepository.save(memberConverter.toEntity(memberModel));
+	}
+
+	private MemberModel findMember(final Long memberId) {
+		return memberRepository
+				.findById(memberId)
+				.map(memberConverter::from)
+				.orElseThrow(NotFoundMemberException::new);
+	}
+
+	public void isAdmin(Long memberId) {
+		if (memberId == 0) {
+			return;
+		}
+		throw new DeniedMemberEditException(memberId);
+	}
+
+	@Override
+	@Transactional
+	public void delete(final Long adminMemberId, final Long memberId) {
+		MemberModel member = findMember(memberId);
+		isAdmin(adminMemberId);
+
+		memberRepository.deleteById(member.getId());
+		applicationEventPublisher.publishEvent(DeletedMemberEvent.of(memberId));
 	}
 }
