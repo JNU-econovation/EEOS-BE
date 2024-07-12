b8c94eb9d5214a971d2b0db80bd9d80a6d6777ed
[CLOUD4] 김다애 <wlsemrdl1212@naver.com>
2024-07-12 20:51:40 +0900

    feat : programRepository 작성

--- eeos/src/main/java/com/blackcompany/eeos/program/persistence/ProgramRepository.java
+++ eeos/src/main/java/com/blackcompany/eeos/program/persistence/ProgramRepository.java
@@ -1,9 +1,11 @@
 package com.blackcompany.eeos.program.persistence;
 
+import com.blackcompany.eeos.program.application.model.ProgramAttendMode;
 import java.sql.Timestamp;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -26,4 +28,9 @@ public interface ProgramRepository extends JpaRepository<ProgramEntity, Long> {
 	@Query(
 			"SELECT p FROM ProgramEntity p WHERE p.programDate >=:now AND p.isDeleted=false ORDER BY p.programDate DESC, p.title ASC ")
 	Page<ProgramEntity> findAllByIng(@Param("now") Timestamp now, Pageable pageable);
+
+	@Modifying
+	@Query("UPDATE ProgramEntity p SET p.attendMode=:attendMode WHERE p.id=:programId")
+	Integer changeAttendMode(
+			@Param("programId") Long programId, @Param("attendMode") ProgramAttendMode attendMode);
 }
