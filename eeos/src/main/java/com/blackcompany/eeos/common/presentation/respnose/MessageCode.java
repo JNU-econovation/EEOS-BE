da6f85c0592514d6b723b3aedcc5553810a386e3
KimJongMin <whdals011203@naver.com>
2024-07-15 11:09:28 +0900

    remove: ATTEND_START 응답 코드 삭제

--- eeos/src/main/java/com/blackcompany/eeos/common/presentation/respnose/MessageCode.java
+++ eeos/src/main/java/com/blackcompany/eeos/common/presentation/respnose/MessageCode.java
@@ -4,8 +4,7 @@ public enum MessageCode {
 	CREATE("201", "생성 성공"),
 	GET("200", "조회 성공"),
 	UPDATE("200", "수정 성공"),
-	DELETE("200", "삭제 성공"),
-	ATTEND_START("200", "출석 모드 변경 성공");
+	DELETE("200", "삭제 성공");
 	private final String code;
 	private final String message;
 
