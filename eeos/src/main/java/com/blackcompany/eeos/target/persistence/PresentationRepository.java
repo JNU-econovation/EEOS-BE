0abac637380df18a92924fdd429d9b30bc0b39b1
Bellmin <71818636+rlajm1203@users.noreply.github.com>
2024-07-15 13:42:25 +0900

    [FIX] 팀 리스트 조회 기능을 보완합니다. (#100)

--- eeos/src/main/java/com/blackcompany/eeos/target/persistence/PresentationRepository.java
+++ eeos/src/main/java/com/blackcompany/eeos/target/persistence/PresentationRepository.java
@@ -1,7 +1,15 @@
 package com.blackcompany.eeos.target.persistence;
 
+import com.blackcompany.eeos.team.persistence.TeamEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PresentationRepository extends JpaRepository<PresentationEntity, Long> {
+
+    @Query("SELECT t FROM TeamEntity t WHERE t.id in (SELECT p.id FROM PresentationEntity p WHERE p.programId=:programId)")
+    List<TeamEntity> findTeamsByProgramId(@Param("programId") Long programId);
 
 }
