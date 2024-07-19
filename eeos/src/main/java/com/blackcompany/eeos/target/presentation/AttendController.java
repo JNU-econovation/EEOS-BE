ddc6990fefeca393569cc12ddd0579eb4852d368
Bellmin <71818636+rlajm1203@users.noreply.github.com>
2024-07-19 16:30:49 +0900

    [UPDATE] PostMapping import (#118)

--- eeos/src/main/java/com/blackcompany/eeos/target/presentation/AttendController.java
+++ eeos/src/main/java/com/blackcompany/eeos/target/presentation/AttendController.java
@@ -17,13 +17,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
