aff6d4e6c38bde9ed1c5a73d5fed09e252ceb4bf
KimJongMin <whdals011203@naver.com>
2024-07-23 16:47:50 +0900

    feat: ProgramQuitUsecase 정의

---
+++ eeos/src/main/java/com/blackcompany/eeos/program/application/usecase/ProgramQuitUsecase.java
@@ -0,0 +1,8 @@
+package com.blackcompany.eeos.program.application.usecase;
+
+import com.blackcompany.eeos.program.application.model.ProgramModel;
+
+public interface ProgramQuitUsecase {
+
+	void pushQuitAttendJob(ProgramModel model);
+}
