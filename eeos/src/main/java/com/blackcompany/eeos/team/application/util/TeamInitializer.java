f878217ca238d5df0834e4476ec98d7abc43ef8d
Bellmin <71818636+rlajm1203@users.noreply.github.com>
2024-10-03 23:41:12 +0900

    [REFACTOR] 이전 학기 팀 리스트 환경 변수에서 가져와 생성 (#164)

--- eeos/src/main/java/com/blackcompany/eeos/team/application/util/TeamInitializer.java
+++ eeos/src/main/java/com/blackcompany/eeos/team/application/util/TeamInitializer.java
@@ -4,9 +4,14 @@ import com.blackcompany.eeos.team.persistence.TeamEntity;
 import com.blackcompany.eeos.team.persistence.TeamRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 
 // 기존 데이터베이스와의 데이터 정합성을 위한 클래스
@@ -17,12 +22,18 @@ public class TeamInitializer implements ApplicationRunner {
 
     private final TeamRepository teamRepository;
 
+    @Value("${eeos.team.list}")
+    private String teamList;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
         System.out.println("-----------Team Initializer-----------");
+        Arrays.stream(teamList.split(",")).forEach(log::info);
         try {
-            TeamEntity defaultTeam = TeamEntity.builder().id(0L).name("임시 활동 팀").status(false).build();
-            teamRepository.save(defaultTeam);
+            Set<TeamEntity> newTeams = Arrays.stream(teamList.split(","))
+                    .map(teamName -> TeamEntity.builder().name(teamName).status(false).build())
+                    .collect(Collectors.toSet());
+            teamRepository.saveAll(newTeams);
             log.info("임시 팀이 생성되었습니다.");
         } catch (Exception e){
             log.error("임시 팀이 생성되지 않았습니다.");
