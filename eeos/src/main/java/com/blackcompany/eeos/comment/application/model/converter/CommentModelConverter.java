2ca7e34c87f450eb649b0341997ff469ff364733
[CLOUD4] 김다애 <wlsemrdl1212@naver.com>
2024-11-28 23:38:47 +0900

    refactor : 변수명 수정

--- eeos/src/main/java/com/blackcompany/eeos/comment/application/model/converter/CommentModelConverter.java
+++ eeos/src/main/java/com/blackcompany/eeos/comment/application/model/converter/CommentModelConverter.java
@@ -16,7 +16,7 @@ public class CommentModelConverter
 				.superCommentId(request.getParentsCommentId())
 				.content(request.getContent())
 				.presentingTeam(request.getTeamId())
-				.isAnonymous(request.getIsAnonymous())
+				.isAnonymous(request.isAnonymous())
 				.build();
 	}
 
