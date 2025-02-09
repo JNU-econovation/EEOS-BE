33783fa9088ccec8cdbbef56a5c7973bc8ba1bdf
mando <mando@krafton.com>
2025-02-09 14:30:54 +0900

    feat : header 포함하여 response 정의

--- eeos/src/main/java/com/blackcompany/eeos/common/presentation/respnose/ApiResponseGenerator.java
+++ eeos/src/main/java/com/blackcompany/eeos/common/presentation/respnose/ApiResponseGenerator.java
@@ -2,6 +2,7 @@ package com.blackcompany.eeos.common.presentation.respnose;
 
 import java.util.List;
 import lombok.experimental.UtilityClass;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
@@ -32,6 +33,14 @@ public class ApiResponseGenerator {
 		return new ApiResponse<>(
 				new ApiResponseBody.FailureBody(
 						String.valueOf(status.value()), code, createErrorMessage(bindingResult)),
+				status);
+	}
+
+	public static ApiResponse<ApiResponseBody.FailureBody> fail(
+			final String message, final String code, final HttpStatus status, final HttpHeaders headers) {
+		return new ApiResponse<>(
+				new ApiResponseBody.FailureBody(String.valueOf(status.value()), code, message),
+				headers,
 				status);
 	}
 
