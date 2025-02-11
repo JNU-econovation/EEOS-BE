0e036a59525e07bcc8ea702824d63bd271778233
rlajm1203 <whdlas011203@naver.com>
2025-02-11 22:53:52 +0900

    test: ProgramDateRangeService 테스트 코드 작성
    - 페이지네이션 테스트

---
+++ eeos/src/test/java/com/blackcompany/eeos/program/application/service/ProgramDateRangeServiceTest.java
@@ -0,0 +1,48 @@
+package com.blackcompany.eeos.program.application.service;
+
+
+import com.blackcompany.eeos.program.application.model.ProgramModel;
+import static org.mockito.Mockito.verify;
+
+import com.blackcompany.eeos.program.persistence.ProgramEntity;
+import com.blackcompany.eeos.program.persistence.ProgramRepository;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.List;
+import org.bouncycastle.util.Times;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+@ExtendWith(MockitoExtension.class)
+public class ProgramDateRangeServiceTest {
+
+    @InjectMocks ProgramDateRangeService programDateRangeService;
+
+    @Mock ProgramRepository programRepository;
+
+    @Test
+    @DisplayName("특정범위_사이의_행사_리스트를_조회한다.")
+    void get_program_date_range() {
+        // given
+        Timestamp startDate = Timestamp.from(Instant.from(LocalDate.of(2024,9,1)));
+        Timestamp endDate = Timestamp.from(Instant.from(LocalDate.of(2024,9,10)));
+        int page = 1;
+        int size = 1;
+        Pageable pageable = PageRequest.of(page, size);
+
+        // when
+        Page<ProgramEntity> pages = programRepository.findByDateRange(startDate, endDate, pageable);
+
+        // then
+        verify(programRepository).findByDateRange(startDate, endDate, pageable);
+
+    }
+}
