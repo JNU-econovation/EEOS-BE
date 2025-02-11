32ee0f91a8121962dcdcac2c43faeb5e5eca57fd
rlajm1203 <whdlas011203@naver.com>
2025-02-11 21:22:01 +0900

    refactor: Builder 추가 및 필드명 변경

--- eeos/src/main/java/com/blackcompany/eeos/target/application/dto/AttendInfoWithProgramResponse.java
+++ eeos/src/main/java/com/blackcompany/eeos/target/application/dto/AttendInfoWithProgramResponse.java
@@ -1,11 +1,13 @@
 package com.blackcompany.eeos.target.application.dto;
 
 import com.blackcompany.eeos.common.support.dto.AbstractResponseDto;
+import lombok.Builder;
 
+@Builder
 public record AttendInfoWithProgramResponse(
         Long programId,
+        String title,
         String programStatus,
-        String programName,
         String attendStatus
 ) implements AbstractResponseDto {
 }
