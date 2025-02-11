5529ab025026ad7f26372d7f40b663e51689249d
rlajm1203 <whdlas011203@naver.com>
2025-02-11 21:22:32 +0900

    feat: AttendInfoProgramResponseConverter 정의

---
+++ eeos/src/main/java/com/blackcompany/eeos/target/application/dto/converter/AttendInfoWithProgramConverter.java
@@ -0,0 +1,20 @@
+package com.blackcompany.eeos.target.application.dto.converter;
+
+import com.blackcompany.eeos.program.application.model.ProgramModel;
+import com.blackcompany.eeos.target.application.dto.AttendInfoWithProgramResponse;
+import com.blackcompany.eeos.target.application.model.AttendModel;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AttendInfoWithProgramConverter {
+
+    public AttendInfoWithProgramResponse from(AttendModel attendModel, ProgramModel programModel){
+        return AttendInfoWithProgramResponse.builder()
+                .attendStatus(attendModel.getStatus())
+                .programId(programModel.getId())
+                .programStatus(programModel.getProgramStatus())
+                .title(programModel.getTitle())
+                .build();
+    }
+
+}
