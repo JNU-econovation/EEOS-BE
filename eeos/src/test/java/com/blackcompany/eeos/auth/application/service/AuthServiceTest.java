c7bf617bc7057341d186fc373cb094dbd17aaba1
kssumin <201566@jnu.ac.kr>
2025-02-12 12:57:10 +0900

    test : 실패 테스트 통과

--- eeos/src/test/java/com/blackcompany/eeos/auth/application/service/AuthServiceTest.java
+++ eeos/src/test/java/com/blackcompany/eeos/auth/application/service/AuthServiceTest.java
@@ -1,13 +1,16 @@
 package com.blackcompany.eeos.auth.application.service;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import com.blackcompany.eeos.auth.application.domain.OauthMemberModel;
 import com.blackcompany.eeos.auth.application.domain.converter.OauthMemberEntityConverter;
 import com.blackcompany.eeos.auth.application.repository.OAuthMemberRepository;
 import com.blackcompany.eeos.auth.fixture.FakeOauthMember;
 import com.blackcompany.eeos.member.application.model.ActiveStatus;
+import com.blackcompany.eeos.member.application.model.MemberModel;
 import com.blackcompany.eeos.member.application.model.converter.MemberEntityConverter;
 import com.blackcompany.eeos.member.application.repository.MemberRepository;
 import com.blackcompany.eeos.member.fixture.MemberFixture;
@@ -17,7 +20,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -34,16 +36,20 @@ class AuthServiceTest {
 	@DisplayName("신규 회원인 경우 oauth에서 가져온 회원 정보를 저장한다.")
 	void login_existing_user() {
 		// given
-		when(oAuthMemberRepository.findByOauthId(FakeOauthMember.oauthMemberModel().getOauthId()))
-				.thenReturn(Optional.ofNullable(null));
-		when(memberRepository.save(Mockito.any())).thenReturn(MemberFixture.멤버_모델(1L, ActiveStatus.AM));
+		OauthMemberModel expectedModel = FakeOauthMember.oauthMemberModel();
+		MemberModel savedMember = MemberFixture.멤버_모델(1L, ActiveStatus.AM);
+
+		when(oAuthMemberRepository.findByOauthId(expectedModel.getOauthId()))
+				.thenReturn(Optional.empty());
+		when(memberRepository.save(any())).thenReturn(savedMember);
+		when(oAuthMemberRepository.save(any())).thenReturn(expectedModel);
 
 		// when
-		authService.authenticate(FakeOauthMember.oauthMemberModel());
+		authService.authenticate(expectedModel);
 
 		// then
 		assertAll(
-				() -> verify(memberRepository).save(Mockito.any()),
-				() -> verify(oAuthMemberRepository).save(Mockito.any()));
+				() -> verify(memberRepository).save(any()),
+				() -> verify(oAuthMemberRepository).save(any()));
 	}
 }
