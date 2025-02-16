f4c7b1bff000e7abcca086dcd147243b26565d5b
kssumin <201566@jnu.ac.kr>
2025-02-17 00:56:33 +0900

    feat : 역직렬화 시 사용할 기본생성자

--- eeos/src/main/java/com/blackcompany/eeos/auth/persistence/OAuthInfo.java
+++ eeos/src/main/java/com/blackcompany/eeos/auth/persistence/OAuthInfo.java
@@ -1,11 +1,15 @@
 package com.blackcompany.eeos.auth.persistence;
 
 import com.blackcompany.eeos.auth.application.domain.OauthServerType;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class OAuthInfo {
 	private String oauthId;
 	private OauthServerType oauthServerType;
