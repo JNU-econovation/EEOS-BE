642a9c2674137cf25b6991019a0c8006262bbae1
[CLOUD4] 김다애 <wlsemrdl1212@naver.com>
2025-02-22 23:57:23 +0900

    feat : 파이어 핑거 로직 수정
    파이어핑거에서 Update_date 와 rank를 기준으로 파이어핑거 순위를 나열하도록 했습니다.

--- eeos/src/main/java/com/blackcompany/eeos/target/application/service/AttendService.java
+++ eeos/src/main/java/com/blackcompany/eeos/target/application/service/AttendService.java
@@ -98,6 +98,11 @@ public class AttendService
 
 		AttendModel changedModel = model.changeStatus(program.getAttendMode().getMode());
 
+		if(changedModel.getStatus().equals("attend")){
+			Long rank = calculateRank(programId);
+			changedModel.setRank(rank);
+		}
+
 		AttendEntity updated = attendRepository.save(attendEntityConverter.toEntity(changedModel));
 
 		String name = queryMemberService.getName(memberId);
@@ -134,8 +139,7 @@ public class AttendService
 	public List<AttendInfoResponse> findFireFingerMembers(final Long programId) {
 		validateExistsProgram(programId);
 
-		List<AttendModel> attendModels = findTop10Attendants(programId);
-
+		List<AttendModel> attendModels = findTop5Attendants(programId);
 
 
 		return attendModels.stream()
@@ -151,9 +155,9 @@ public class AttendService
 				.collect(Collectors.toList());
 	}
 
-	private List<AttendModel> findTop10Attendants(Long programId) {
+	private List<AttendModel> findTop5Attendants(Long programId) {
 		return attendRepository
-				.findTop10ByProgramIdAndStatusOrderByUpdatedDateAsc(programId, AttendStatus.ATTEND).stream()
+				.findTop5ByProgramIdAndStatusOrderByUpdatedDateAscRankAsc(programId, AttendStatus.ATTEND).stream()
 				.map(attendEntityConverter::from)
 				.collect(Collectors.toList());
 	}
@@ -249,6 +253,10 @@ public class AttendService
 		if (!programRepository.existsById(programId)) {
 			throw new NotFoundProgramException(programId);
 		}
+	}
+
+	private Long calculateRank(Long programId){
+		return attendRepository.countAttendStatusByProgramIdAndStatus(programId, AttendStatus.ATTEND) +1;
 	}
 
 
