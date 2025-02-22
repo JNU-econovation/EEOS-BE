36c2412424f9c6972dd4cfbe47645ada50d28425
rlajm1203 <whdlas011203@naver.com>
2025-02-22 23:04:23 +0900

    feat: RequestScope 클래스 구현

---
+++ eeos/src/main/java/com/blackcompany/eeos/common/utils/RequestScope.java
@@ -0,0 +1,21 @@
+package com.blackcompany.eeos.common.utils;
+
+
+// HTTP Request 를 보낸 사용자의 정보를 저장하는 클래스
+public class RequestScope {
+
+    public static final ThreadLocal<Long> requesterInfo = new ThreadLocal<>();
+
+    public static Long getMemberId() {
+        return requesterInfo.get();
+    }
+
+    public static void setMemberId(Long memberId) {
+        requesterInfo.set(memberId);
+    }
+
+    public static void clear() {
+        requesterInfo.remove();
+    }
+
+}
