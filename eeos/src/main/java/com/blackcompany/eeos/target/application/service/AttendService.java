3c853c69880912e0506ce8d2e4a0d08e34834a8a
rlajm1203 <whdlas011203@naver.com>
2025-02-27 12:03:43 +0900

    feat: 출석 요약 정보 조회 기능 추가 및 사용하지 않는 메소드 제거

--- eeos/src/main/java/com/blackcompany/eeos/target/application/service/AttendService.java
+++ eeos/src/main/java/com/blackcompany/eeos/target/application/service/AttendService.java
@@ -1,5 +1,7 @@
 package com.blackcompany.eeos.target.application.service;
 
+import com.blackcompany.eeos.common.utils.DateConverter;
+import com.blackcompany.eeos.common.utils.RequestScope;
 import com.blackcompany.eeos.member.application.model.ActiveStatus;
 import com.blackcompany.eeos.member.application.model.MemberModel;
 import com.blackcompany.eeos.member.application.model.converter.MemberEntityConverter;
@@ -32,12 +34,14 @@ import com.blackcompany.eeos.target.application.exception.NotStartAttendExceptio
 import com.blackcompany.eeos.target.application.model.AttendModel;
 import com.blackcompany.eeos.target.application.model.AttendStatus;
 import com.blackcompany.eeos.target.application.model.converter.AttendEntityConverter;
+import com.blackcompany.eeos.target.application.support.AttendCountCalculate;
 import com.blackcompany.eeos.target.application.usecase.ChangeAttendStatusUsecase;
 import com.blackcompany.eeos.target.application.usecase.GetAttendAllInfoSortActiveStatusUsecase;
 import com.blackcompany.eeos.target.application.usecase.GetAttendStatusUsecase;
 import com.blackcompany.eeos.target.application.usecase.GetAttendantInfoUsecase;
 import com.blackcompany.eeos.target.persistence.AttendEntity;
 import com.blackcompany.eeos.target.persistence.AttendRepository;
+import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
@@ -70,6 +74,7 @@ public class AttendService
 	private final ProgramDateRangeService programDateRangeService;
 	private final ProgramRepository programRepository;
 	private final ProgramEntityConverter programEntityConverter;
+	private final AttendCountCalculate attendCountCalculate;
 
 	@Override
 	public List<AttendInfoResponse> findAttendInfo(final Long programId) {
@@ -199,26 +204,28 @@ public class AttendService
 
 	@Override
 	public AttendSummaryInfoResponse getMyAttendSummary() {
+		Long memberId = RequestScope.getMemberId();
 
-		return null;
+		Long startDate = (long) DateConverter.toEpochSecond(LocalDate.of(2025,3,1)).getNanos() / 1000;
+		Long endDate = (long) DateConverter.toEpochSecond(LocalDate.of(2025,7,25)).getNanos() / 1000;
+
+		List<ProgramModel> programs = programDateRangeService.getPrograms(startDate, endDate);
+
+		List<AttendModel> attends = findMyAttends(programs);
+
+		Long attendCount = attendCountCalculate.attendCount(attends);
+		Long absentCount = attendCountCalculate.absentCount(attends);
+		Long penaltyPoint = attendCountCalculate.penaltyPoint(attends);
+		Long lateCount = attendCountCalculate.lateCount(attends);
+
+		return new AttendSummaryInfoResponse(memberId, attendCount, absentCount, penaltyPoint);
 	}
 
-	private void validateParameter(Long startDate, Long endDate, Integer size, Integer page) {
-		if (startDate == null || endDate == null || size == null || page == null) {
-			throw new IllegalArgumentException();
-		}
-
-		if (startDate > endDate) {
-			throw new IllegalArgumentException();
-		}
-
-		if (size < 0 || page < 0) {
-			throw new IllegalArgumentException();
-		}
-
-		if (size == 0 || page == 0) {
-			throw new IllegalArgumentException();
-		}
+	private List<AttendModel> findMyAttends(List<ProgramModel> programs) {
+		Long memberId = RequestScope.getMemberId();
+		return attendRepository.findByProgramIdsAndMemberId(
+						programs.stream().map(ProgramModel::getId).collect(Collectors.toList()), memberId)
+				.stream().map(attendEntityConverter::from).toList();
 	}
 
 	private void validateAttend(ProgramModel programModel, AttendModel attendModel) {
