58c7b917febb827d5757b6ef56fea2ee3f27de74
rlajm1203 <whdlas011203@naver.com>
2025-02-27 12:00:32 +0900

    feat: AttendCountCalculate 정의
    - 출석 횟수를 계산하는 계산기

---
+++ eeos/src/main/java/com/blackcompany/eeos/target/application/support/AttendCountCalculate.java
@@ -0,0 +1,35 @@
+package com.blackcompany.eeos.target.application.support;
+
+import com.blackcompany.eeos.target.application.model.AttendModel;
+import com.blackcompany.eeos.target.application.model.AttendStatus;
+import java.util.List;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AttendCountCalculate {
+
+    public Long attendCount(List<AttendModel> attendModels){
+        return attendModels.stream()
+                .filter(attend -> AttendStatus.find(attend.getStatus()).equals(AttendStatus.ATTEND))
+                .count();
+    }
+
+    public Long absentCount(List<AttendModel> attendModels){
+        return attendModels.stream()
+                .filter(attend -> AttendStatus.find(attend.getStatus()).equals(AttendStatus.ABSENT))
+                .count();
+    }
+
+    public Long lateCount(List<AttendModel> attendModels){
+        return attendModels.stream()
+                .filter(attend -> AttendStatus.find(attend.getStatus()).equals(AttendStatus.LATE))
+                .count();
+    }
+
+    public Long penaltyPoint(List<AttendModel> attendModels){
+        return attendModels.stream().reduce(0L , (i, attend) -> i + attend.getPenaltyScore(), Long::sum);
+    }
+
+
+
+}
