61b47b628c62d3e641b4d90ec8602d2450e172f5
kssumin <201566@jnu.ac.kr>
2025-03-01 11:40:13 +0900

    feat : 참석 가중치 설정 entity, repository

---
+++ eeos/src/main/java/com/blackcompany/eeos/target/application/model/SignType.java
@@ -0,0 +1,28 @@
+package com.blackcompany.eeos.target.application.model;
+
+import com.blackcompany.eeos.target.application.exception.NotFoundAttendStatusException;
+import java.util.Arrays;
+import lombok.Getter;
+
+@Getter
+public enum SignType {
+	PLUS("plus"),
+	MINUS("minus");
+
+	private final String type;
+
+	SignType(String type) {
+		this.type = type;
+	}
+
+	public static SignType find(String type) {
+		return Arrays.stream(SignType.values())
+				.filter(value -> value.getType().equals(type))
+				.findAny()
+				.orElseThrow(() -> new NotFoundAttendStatusException(type)); // TODO : 이거 변경하기
+	}
+
+	public static boolean isSame(String source, AttendStatus target) {
+		return target.getStatus().equals(source);
+	}
+}
