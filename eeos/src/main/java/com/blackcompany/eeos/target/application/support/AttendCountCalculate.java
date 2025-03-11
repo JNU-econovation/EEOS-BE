4ab2474fe19f16e5b6a7ccbb890d3cdf7814cc2f
rlajm1203 <whdlas011203@naver.com>
2025-03-12 02:13:24 +0900

    style: spotlessApply

--- eeos/src/main/java/com/blackcompany/eeos/target/application/support/AttendCountCalculate.java
+++ eeos/src/main/java/com/blackcompany/eeos/target/application/support/AttendCountCalculate.java
@@ -1,17 +1,14 @@
 package com.blackcompany.eeos.target.application.support;
 
 import com.blackcompany.eeos.target.application.model.AttendModel;
-import com.blackcompany.eeos.target.application.model.AttendStatus;
 import java.util.List;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AttendCountCalculate {
-	
+
 	public Long countByStatus(String status, List<AttendModel> attendModels) {
-		return attendModels.stream()
-				.filter(attend -> attend.getStatus().equals(status))
-				.count();
+		return attendModels.stream().filter(attend -> attend.getStatus().equals(status)).count();
 	}
 
 	public Long penaltyPoint(List<AttendModel> attendModels) {
