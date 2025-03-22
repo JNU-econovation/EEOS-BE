1950a125fb2ba455d873b73a276d3140f2a90321
[CLOUD4] 김다애 <wlsemrdl1212@naver.com>
2025-03-23 00:38:45 +0900

    fix : rank 설정을 위한 ProgramRankCounterRepository 추가

---
+++ eeos/src/main/java/com/blackcompany/eeos/target/persistence/ProgramRankCounterRepository.java
@@ -0,0 +1,19 @@
+package com.blackcompany.eeos.target.persistence;
+
+
+import jakarta.persistence.LockModeType;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProgramRankCounterRepository extends JpaRepository<ProgramRankCounterEntity, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT c FROM ProgramRankCounterEntity c WHERE c.programId = :programId")
+    Optional<ProgramRankCounterEntity> findByProgramIdForUpdate(@Param("programId") Long programId);
+
+}
