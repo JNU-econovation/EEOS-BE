a7e5a737b0387bc522807aef19ef44f8ee8b14d0
[CLOUD4] 김다애 <wlsemrdl1212@naver.com>
2025-03-23 00:38:35 +0900

    fix : rank 설정을 위한 ProgramRankCounterEntity 추가

---
+++ eeos/src/main/java/com/blackcompany/eeos/target/persistence/ProgramRankCounterEntity.java
@@ -0,0 +1,46 @@
+package com.blackcompany.eeos.target.persistence;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@ToString
+@SuperBuilder(toBuilder = true)
+@Entity
+@Table(name = ProgramRankCounterEntity.ENTITY_PREFIX)
+public class ProgramRankCounterEntity {
+    public static final String ENTITY_PREFIX = "program_rank_counter";
+
+    @Id
+    @Column(name = ENTITY_PREFIX+"_id", nullable = false)
+    private Long programId;
+
+    @Column(name = ENTITY_PREFIX+"_nextRank", nullable = false)
+    private Long nextRank;
+
+    public void setProgramId(Long programId) {
+        this.programId = programId;
+    }
+
+    public void setNextRank(Long nextRank) {
+        this.nextRank = nextRank;
+    }
+
+    public void incrementNextRank() {
+        this.nextRank = this.nextRank + 1;
+    }
+
+}
