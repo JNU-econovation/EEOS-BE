8400f546863fe36de1befadf69ba67535f5b469e
rlajm1203 <rlajm1203@naver.com>
2025-03-27 00:25:17 +0900

    feat: findAllByMemberIdAndCreatedDateGreaterThan 메소드 추가

--- eeos/src/main/java/com/blackcompany/eeos/target/persistence/AttendRepository.java
+++ eeos/src/main/java/com/blackcompany/eeos/target/persistence/AttendRepository.java
@@ -2,6 +2,7 @@ package com.blackcompany.eeos.target.persistence;
 
 import com.blackcompany.eeos.target.application.model.AttendStatus;
 import jakarta.persistence.LockModeType;
+import java.sql.Timestamp;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -54,4 +55,8 @@ public interface AttendRepository extends JpaRepository<AttendEntity, Long> {
 			"SELECT a FROM AttendEntity a WHERE a.programId IN :programIds AND a.memberId=:memberId AND a.isDeleted=false")
 	List<AttendEntity> findByProgramIdsAndMemberId(
 			@Param("programIds") List<Long> programIds, @Param("memberId") Long memberId);
+
+	@Query("SELECT a FROM AttendEntity a WHERE a.memberId = :memberId AND a.isDeleted = false AND a.createdDate > :startDate AND a.createdDate < :endDate")
+	List<AttendEntity> findAllByMemberIdAndCreatedDateGreaterThan(
+			@Param("memberId") Long memberId, @Param("createdDate") Timestamp createdDate, @Param("endDate") Timestamp endDate);
 }
