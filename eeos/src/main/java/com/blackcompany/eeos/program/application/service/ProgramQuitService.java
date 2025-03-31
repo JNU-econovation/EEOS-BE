245b944d5672be0d00a1981c46bed4c052604edc
rlajm1203 <rlajm1203@naver.com>
2025-03-31 16:25:50 +0900

    refactor: 이벤트 처리로 하지 않고 동기로 처리
    - 출석 체크 자동 종료가 완료된 경우에만 삭제하도록 수정

--- eeos/src/main/java/com/blackcompany/eeos/program/application/service/ProgramQuitService.java
+++ eeos/src/main/java/com/blackcompany/eeos/program/application/service/ProgramQuitService.java
@@ -1,12 +1,20 @@
 package com.blackcompany.eeos.program.application.service;
 
 import com.blackcompany.eeos.common.utils.DateConverter;
+import com.blackcompany.eeos.program.application.model.ProgramAttendMode;
 import com.blackcompany.eeos.program.application.model.ProgramModel;
 import com.blackcompany.eeos.program.application.support.DelayedQueue;
 import com.blackcompany.eeos.program.application.usecase.ProgramQuitUsecase;
+import com.blackcompany.eeos.program.persistence.ProgramRepository;
 import com.blackcompany.eeos.target.application.event.EndAttendModeEvent;
+import com.blackcompany.eeos.target.application.model.AttendStatus;
+import com.blackcompany.eeos.target.persistence.AttendRepository;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,11 +29,12 @@ import org.springframework.transaction.annotation.Transactional;
 public class ProgramQuitService implements ProgramQuitUsecase {
 
 	private final DelayedQueue delayedQueue;
-	private final ApplicationEventPublisher eventPublisher;
+	private final AttendRepository attendRepository;
+	private final ProgramRepository programRepository;
 	private final String KEY = "quit_program_reservation";
 
 	@Override
-	public void pushQuitAttendJob(ProgramModel model) {
+	public void reserveQuitProgram(ProgramModel model) {
 		// programDate 를 score 로 사용
 		long programDate = model.getProgramDate().getTime() / 1000;
 
@@ -35,22 +44,44 @@ public class ProgramQuitService implements ProgramQuitUsecase {
 	@Transactional
 	@Scheduled(cron = "0 0 0 * * *")
 	public void quitAttend() {
+		log.info("출석 체크 자동 종료 시작");
 		try {
-			Set<Long> jobs = getReadyTasks();
-			eventPublisher.publishEvent(EndAttendModeEvent.of(jobs));
+			long programDate = DateConverter.toEpochSecond(LocalDate.now()).getTime();
+
+			Set<Long> jobs = getReadyTasks(programDate);
+
+			Set<Long> completedIds = doQuit(jobs);
+
+			removeCompleteTask(completedIds);
+
 		} catch (Exception e) {
-			log.error("[ProgramQuitService] 행사 자동 종료 중 에러가 발생하였습니다. {}", e.getMessage());
+			log.error("행사 자동 종료 중 에러가 발생하였습니다. {}", e.getMessage());
 			throw e;
 		}
 	}
 
-	private Set<Long> getReadyTasks() {
-		long programDate = DateConverter.toEpochSecond(LocalDate.now()).getTime();
+	private Set<Long> doQuit(Set<Long> programIds) {
+		Set<Long> completedIds = new HashSet<>();
+		for (Long id : programIds) {
+			log.info("출석 체크 자동 종료 (programId : {})", id);
 
+			programRepository.changeAttendMode(id, ProgramAttendMode.END);
+			attendRepository.updateAttendStatusByProgramId(id, AttendStatus.NONRESPONSE, AttendStatus.ABSENT);
+
+			completedIds.add(id);
+		}
+		return completedIds;
+	}
+
+	private Set<Long> getReadyTasks(long programDate) {
 		Set<Long> jobs =
 				delayedQueue.getReadyTasks(KEY, (double) programDate).stream()
 						.map(id -> Long.parseLong(id.toString()))
 						.collect(Collectors.toSet());
 		return jobs;
+	}
+
+	private void removeCompleteTask(Set<Long> programIds) {
+		delayedQueue.removeByValue(KEY, programIds);
 	}
 }
