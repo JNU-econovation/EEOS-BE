4df91c725e458d0038a6dd3ed5d287e5ba322a79
rlajm1203 <rlajm1203@naver.com>
2025-06-26 17:45:05 +0900

    feat: AccessToken 필터 정의
    - http Auth 헤더에서 토큰을 추출하여 인증 객체로 변환하는 로직 담당

---
+++ eeos/src/main/java/com/blackcompany/eeos/config/security/AccessTokenEntryPoint.java
@@ -0,0 +1,25 @@
+package com.blackcompany.eeos.config.security;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+// JwtAuthentication이 없을경우 실행됨 (유효한 access token이 없음)
+@Component
+public class AccessTokenEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AuthenticationException authException)
+            throws IOException {
+        response.setContentType("application/json;charset=UTF-8");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 401
+        response.getWriter().write("{\"message\": \"인증이 필요합니다\", \"error_code\": \"2000\"}");
+        response.getWriter().flush();
+    }
+}
---
+++ eeos/src/main/java/com/blackcompany/eeos/config/security/AccessTokenFilter.java
@@ -0,0 +1,64 @@
+package com.blackcompany.eeos.config.security;
+
+import com.blackcompany.eeos.auth.application.domain.token.TokenResolver;
+import com.blackcompany.eeos.auth.presentation.support.TokenExtractor;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+public class AccessTokenFilter extends OncePerRequestFilter {
+
+    private final TokenExtractor tokenExtractor;
+    private final TokenResolver tokenResolver;
+
+    public AccessTokenFilter(
+            @Qualifier("header") TokenExtractor tokenExtractor, TokenResolver tokenResolver) {
+        this.tokenExtractor = tokenExtractor;
+        this.tokenResolver = tokenResolver;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String token = tokenExtractor.extract(request); // tokenExtractor 에서 Auth 헤더가 있는지 검사함
+
+        createAuthentication(token)
+                .ifPresentOrElse(this::setAuthentication, SecurityContextHolder::clearContext);
+
+        filterChain.doFilter(request, response);
+    }
+
+    private Optional<JwtAuthentication> createAuthentication(String token) {
+        Optional<Long> memberId = parseToken(token);
+
+        // TODO: ROLE
+        return memberId.map(
+                id ->
+                        new JwtAuthentication(
+                                id, Collections.singletonList(new SimpleGrantedAuthority("user"))));
+    }
+
+    private Optional<Long> parseToken(String token) {
+        try {
+            return Optional.of(tokenResolver.getUserDataByAccessToken(token));
+        } catch (Exception e) {
+            return Optional.empty();
+        }
+    }
+
+    private void setAuthentication(JwtAuthentication authentication) {
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+}
