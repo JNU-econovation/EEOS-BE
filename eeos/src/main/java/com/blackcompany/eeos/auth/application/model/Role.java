e1f350fe0ae03d2fb161e5fee241750e9550a12a
rlajm1203 <rlajm1203@naver.com>
2025-06-28 02:01:39 +0900

    feat: Role enum 클래스 정의

---
+++ eeos/src/main/java/com/blackcompany/eeos/auth/application/model/Role.java
@@ -0,0 +1,17 @@
+package com.blackcompany.eeos.auth.application.model;
+
+import lombok.Getter;
+
+@Getter
+public enum Role {
+
+    ADMIN("ADMIN"),
+    USER("USER");
+
+    private String role;
+
+    Role(String role){
+        this.role = role;
+    }
+
+}
