33dd1ca25fc8a2b3bd16c1d055f4d520a868d8b3
Bellmin <whdals011203@naver.com>
2025-07-21 00:44:15 +0900

    feat: JwtTestUtil stringKey, accessSecretKey 추가

--- eeos/src/test/java/com/blackcompany/eeos/auth/application/domain/token/JwtTestUtil.java
+++ eeos/src/test/java/com/blackcompany/eeos/auth/application/domain/token/JwtTestUtil.java
@@ -7,12 +7,14 @@ import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.time.Instant;
 import java.util.Date;
+import java.util.stream.IntStream;
 
 public class JwtTestUtil {
 
     private static final Date now = Date.from(Instant.now());
     private static final long accessValidTime = 3600 * 1000;
-    private static final Key accessSecretKey = Keys.hmacShaKeyFor("accessSecretKey".getBytes(StandardCharsets.UTF_8));
+    private static final String stringKey = IntStream.generate(() -> 1).limit(120).toString();
+    private static final Key accessSecretKey = Keys.hmacShaKeyFor(stringKey.getBytes(StandardCharsets.UTF_8));
 
     public static String createToken(Long memberId, String role){
 
