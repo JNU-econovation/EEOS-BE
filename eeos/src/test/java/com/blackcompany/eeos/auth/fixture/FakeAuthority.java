018346b833e9bb3b5d69767e41dc905ed71f065b
Bellmin <whdals011203@naver.com>
2025-07-23 22:59:36 +0900

    refactor: 테스트 코드 수정
    - Role Enum 적용

--- eeos/src/test/java/com/blackcompany/eeos/auth/fixture/FakeAuthority.java
+++ eeos/src/test/java/com/blackcompany/eeos/auth/fixture/FakeAuthority.java
@@ -1,10 +1,11 @@
 package com.blackcompany.eeos.auth.fixture;
 
 import com.blackcompany.eeos.auth.application.model.AuthorityModel;
+import com.blackcompany.eeos.auth.application.model.Role;
 
 public class FakeAuthority {
 
-	public static AuthorityModel authorityModel(Long id, Long memberId, String role) {
-		return AuthorityModel.builder().id(id).memberId(memberId).name(role).build();
+	public static AuthorityModel authorityModel(Long id, Long memberId, Role role) {
+		return AuthorityModel.builder().id(id).memberId(memberId).role(role).build();
 	}
 }
