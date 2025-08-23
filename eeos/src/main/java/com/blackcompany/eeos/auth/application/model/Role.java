a61cef26099ea69d97eaebb4ad3c00b4fe6f2791
Bellmin <whdals011203@naver.com>
2025-08-23 20:10:26 +0900

    feat: findRole 메소드 추가

--- eeos/src/main/java/com/blackcompany/eeos/auth/application/model/Role.java
+++ eeos/src/main/java/com/blackcompany/eeos/auth/application/model/Role.java
@@ -1,15 +1,27 @@
 package com.blackcompany.eeos.auth.application.model;
 
+import java.util.Arrays;
 import lombok.Getter;
 
 @Getter
 public enum Role {
 	ROLE_ADMIN("ADMIN"),
-	ROLE_USER("USER");
+	ROLE_USER("USER"),
+	ROLE_PRESIDENT("PRESIDENT"),
+	ROLE_DPT_MARKETING("MARKETING"),
+	ROLE_DPT_MANAGEMENT("MANAGEMENT"),
+	ROLE_DPT_EVENT("EVENT");
 
-	private String role;
+	private String role; // 시스템 내에 존재하는 role을 찾을 때, 이 문자열을 기준으로 찾습니다.
 
 	Role(String role) {
 		this.role = role;
+	}
+
+	public static Role findRole(String role){
+		return Arrays.stream(Role.values())
+				.filter(obj -> obj.getRole().equals(role))
+				.findFirst()
+				.orElseThrow(IllegalArgumentException::new);
 	}
 }
