2b519ba826035140cb835bdd038d2724ab605440
Bellmin <whdals011203@naver.com>
2025-08-24 12:35:26 +0900

    feat: DepartmentUsecase, DepartmentService 구현

---
+++ eeos/src/main/java/com/blackcompany/eeos/member/application/service/DepartmentService.java
@@ -0,0 +1,35 @@
+package com.blackcompany.eeos.member.application.service;
+
+import com.blackcompany.eeos.member.application.model.Department;
+import com.blackcompany.eeos.member.application.model.MemberModel;
+import com.blackcompany.eeos.member.application.repository.MemberRepository;
+import com.blackcompany.eeos.member.application.usecase.DepartmentUsecase;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DepartmentService implements DepartmentUsecase {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public void changeDepartment(Long memberId, String to) {
+        MemberModel member = memberRepository.findById(memberId);
+        Department depart = Department.findDepartment(to);
+        member.updateDepartment(depart);
+    }
+
+    public List<Department> getAllDepartments() {
+        return Department.getAllDepartments();
+    }
+
+    public Department getDepartment(String name) {
+        return Department.findDepartment(name);
+    }
+
+    public Department getDepartmentById(Long id) {
+        return Department.findById(id);
+    }
+}
