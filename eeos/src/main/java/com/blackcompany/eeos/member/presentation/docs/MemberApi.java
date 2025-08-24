6fbc5dadef6a21f975bdb58684033f84d203fb56
Bellmin <whdals011203@naver.com>
2025-08-24 15:25:16 +0900

    feat: swagger 명세 추가

--- eeos/src/main/java/com/blackcompany/eeos/member/presentation/docs/MemberApi.java
+++ eeos/src/main/java/com/blackcompany/eeos/member/presentation/docs/MemberApi.java
@@ -4,12 +4,16 @@ import com.blackcompany.eeos.auth.presentation.support.Member;
 import com.blackcompany.eeos.common.presentation.response.ApiResponse;
 import com.blackcompany.eeos.common.presentation.response.ApiResponseBody.SuccessBody;
 import com.blackcompany.eeos.member.application.dto.ChangeActiveStatusRequest;
+import com.blackcompany.eeos.member.application.dto.ChangeDepartmentRequest;
 import com.blackcompany.eeos.member.application.dto.CommandMemberResponse;
+import com.blackcompany.eeos.member.application.dto.DepartmentResponse;
 import com.blackcompany.eeos.member.application.dto.QueryMemberResponse;
 import com.blackcompany.eeos.member.application.dto.QueryMembersResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import java.util.List;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -39,4 +43,11 @@ public interface MemberApi {
 	@Operation(summary = "본인(회원 1인)의 활동 상태 가져오기", description = "액세스 토큰에서 회원 ID를 추출하여 회원 상태를 가져온다.")
 	ApiResponse<SuccessBody<QueryMemberResponse>> findMemberByActiveStatus(
 			@Parameter(hidden = true) @Member Long memberId);
+
+
+	@Operation(summary = "관리자_회원 부서 변경", description = "회원의 부서를 변경합니다.")
+	ApiResponse<SuccessBody<Void>> changeDepartment(@RequestBody @Valid ChangeDepartmentRequest request);
+
+	@Operation(summary = "관리자_부서 리스트 조회", description = "동아리 내의 부서 리스트를 반환합니다.")
+	ApiResponse<SuccessBody<List<DepartmentResponse>>> getDepartments();
 }
