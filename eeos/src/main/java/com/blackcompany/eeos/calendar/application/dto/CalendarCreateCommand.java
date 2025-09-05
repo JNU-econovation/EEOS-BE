9a50965f47bc8e981d32d9acc03948df40ad034d
Bellmin <whdals011203@naver.com>
2025-09-05 11:13:40 +0900

    refactor: 시간 필드, ms 단위로 변경

--- eeos/src/main/java/com/blackcompany/eeos/calendar/application/dto/CalendarCreateCommand.java
+++ eeos/src/main/java/com/blackcompany/eeos/calendar/application/dto/CalendarCreateCommand.java
@@ -1,13 +1,12 @@
 package com.blackcompany.eeos.calendar.application.dto;
 
 import com.blackcompany.eeos.common.support.dto.AbstractResponseDto;
-import java.time.LocalDateTime;
 
 public record CalendarCreateCommand(
         String title,
         String url,
         String type,
-        LocalDateTime startAt,
-        LocalDateTime endAt
+        Long startAt,
+        Long endAt
 ) implements AbstractResponseDto {
 }
--- eeos/src/main/java/com/blackcompany/eeos/calendar/application/service/CalendarCommandService.java
+++ eeos/src/main/java/com/blackcompany/eeos/calendar/application/service/CalendarCommandService.java
@@ -5,9 +5,11 @@ import com.blackcompany.eeos.calendar.application.model.CalendarModel;
 import com.blackcompany.eeos.calendar.application.repository.CalendarRepository;
 import com.blackcompany.eeos.calendar.application.usecase.CalendarCreateUsecase;
 import com.blackcompany.eeos.calendar.application.validator.CalendarValidator;
+import com.blackcompany.eeos.common.utils.DateConverter;
 import com.blackcompany.eeos.common.utils.RequestScope;
 import com.blackcompany.eeos.member.application.model.MemberModel;
 import com.blackcompany.eeos.member.application.repository.MemberRepository;
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -35,8 +37,9 @@ public class CalendarCommandService implements CalendarCreateUsecase {
     }
 
     private CalendarModel newCalendar(CalendarCreateCommand command, Long memberId){
+        LocalDateTime startAt = DateConverter.toLocalDateTime(command.startAt());
+        LocalDateTime endAt = DateConverter.toLocalDateTime(command.endAt());
         return CalendarModel.create(
-                command.title(), command.startAt(),
-                command.endAt(), command.type(), command.url(), memberId);
+                command.title(), startAt, endAt, command.type(), command.url(), memberId);
     }
 }
