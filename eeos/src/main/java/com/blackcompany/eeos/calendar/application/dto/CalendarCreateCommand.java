dcab43e4643a8d745a7835920be73824ae28a03a
Bellmin <whdals011203@naver.com>
2025-09-10 23:26:59 +0900

    refactor: 잘못된 DTO impl 수정

--- eeos/src/main/java/com/blackcompany/eeos/calendar/application/dto/CalendarCreateCommand.java
+++ eeos/src/main/java/com/blackcompany/eeos/calendar/application/dto/CalendarCreateCommand.java
@@ -1,6 +1,6 @@
 package com.blackcompany.eeos.calendar.application.dto;
 
-import com.blackcompany.eeos.common.support.dto.AbstractResponseDto;
+import com.blackcompany.eeos.common.support.dto.AbstractRequestDto;
 
 public record CalendarCreateCommand(String title, String url, String type, Long startAt, Long endAt)
-		implements AbstractResponseDto {}
+		implements AbstractRequestDto {}
