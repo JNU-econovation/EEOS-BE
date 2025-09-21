7573d1f9f19c233e2092951f334862fb7ebdd8d3
Bellmin <whdals011203@naver.com>
2025-09-21 23:41:00 +0900

    feat: ADMIN 주간발표 행사 생성 시, 캘린더에 반영로직 추가

---
+++ eeos/src/main/java/com/blackcompany/eeos/program/application/event/CreatedProgramEventListener.java
@@ -0,0 +1,45 @@
+package com.blackcompany.eeos.program.application.event;
+
+import com.blackcompany.eeos.calendar.application.dto.CalendarCreateCommand;
+import com.blackcompany.eeos.calendar.application.model.CalendarType;
+import com.blackcompany.eeos.calendar.application.usecase.CreateCalendarUsecase;
+import com.blackcompany.eeos.program.application.exception.NotFoundProgramException;
+import com.blackcompany.eeos.program.application.model.ProgramModel;
+import com.blackcompany.eeos.program.application.model.converter.ProgramEntityConverter;
+import com.blackcompany.eeos.program.persistence.ProgramRepository;
+import java.util.Locale;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+@Component
+@RequiredArgsConstructor
+public class CreatedProgramEventListener {
+
+    private final CreateCalendarUsecase calendarUsecase;
+    private final ProgramRepository programRepository;
+    private final ProgramEntityConverter entityConverter;
+
+    @Transactional
+    @TransactionalEventListener(value = CreatedProgramEvent.class, phase = TransactionPhase.AFTER_COMMIT)
+    public void createCalendar(CreatedProgramEvent event){
+        Long programId = event.getProgramId();
+
+        ProgramModel program = entityConverter.from(programRepository.findById(programId)
+                .orElseThrow(() -> new NotFoundProgramException(programId)));
+
+        Long programDateMilli = program.getProgramDate().toInstant().toEpochMilli();
+
+        CalendarCreateCommand command = new CalendarCreateCommand(
+                program.getTitle(),
+                null,
+                CalendarType.PRESENTATION.name().toLowerCase(Locale.ROOT),
+                programDateMilli,
+                programDateMilli);
+
+        calendarUsecase.create(command);
+    }
+
+}
