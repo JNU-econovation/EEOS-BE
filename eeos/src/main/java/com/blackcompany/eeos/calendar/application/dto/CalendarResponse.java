77a04b7f7f0a789920f4051ffbfb8a8bdf67fabb
Bellmin <whdals011203@naver.com>
2025-09-21 23:02:14 +0900

    [REFACTOR] CalendarResponse Type 소문자로 변경 (#282)

--- eeos/src/main/java/com/blackcompany/eeos/calendar/application/dto/CalendarResponse.java
+++ eeos/src/main/java/com/blackcompany/eeos/calendar/application/dto/CalendarResponse.java
@@ -3,6 +3,7 @@ package com.blackcompany.eeos.calendar.application.dto;
 import com.blackcompany.eeos.calendar.application.model.CalendarModel;
 import com.blackcompany.eeos.common.support.dto.AbstractResponseDto;
 import com.blackcompany.eeos.common.utils.DateConverter;
+import java.util.Locale;
 
 public record CalendarResponse(
 		Long id, String title, String url, String type, Long startAt, Long endAt, String writer)
@@ -11,7 +12,7 @@ public record CalendarResponse(
 	public static CalendarResponse toResponse(CalendarModel model, String writerName) {
 		Long startAt = DateConverter.toMillis(model.getStartAt());
 		Long endAt = DateConverter.toMillis(model.getEndAt());
-		String type = model.getType().name();
+		String type = model.getType().name().toLowerCase(Locale.ROOT);
 
 		return new CalendarResponse(
 				model.getId(), model.getTitle(), model.getUrl(), type, startAt, endAt, writerName);
