107381374a2bd660dd26abc9bc52d83871fc7967
dongE <wlsemrdl1212@naver.com>
2026-01-04 17:54:06 +0900

    feat : 알림토큰 삭제 권한, not found 예외 작성

---
+++ eeos/src/main/java/com/blackcompany/eeos/notification/application/exception/DeniedDeletePushTokenException.java
@@ -0,0 +1,19 @@
+package com.blackcompany.eeos.notification.application.exception;
+
+import org.springframework.http.HttpStatus;
+
+import com.blackcompany.eeos.common.exception.BusinessException;
+
+public class DeniedDeletePushTokenException extends BusinessException {
+  private static final String FAIL_CODE = "5008";
+
+  public DeniedDeletePushTokenException() {
+		super(FAIL_CODE, HttpStatus.UNAUTHORIZED);
+  }
+
+  @Override
+  public String getMessage() {
+    return "토큰 수정 권한이 없습니다.";
+  }
+
+}
---
+++ eeos/src/main/java/com/blackcompany/eeos/notification/application/exception/NotFoundPushTokenException.java
@@ -0,0 +1,21 @@
+package com.blackcompany.eeos.notification.application.exception;
+
+import org.springframework.http.HttpStatus;
+
+import com.blackcompany.eeos.common.exception.BusinessException;
+
+public class NotFoundPushTokenException extends BusinessException {
+
+	private static final String FAIL_CODE = "5007";
+	private final String pushToken;
+	public NotFoundPushTokenException(String pushToken) {
+
+		super(FAIL_CODE, HttpStatus.NOT_FOUND);
+		this.pushToken = pushToken;
+	}
+
+	@Override
+	public String getMessage() {
+		return String.format("%s 는 존재하지 않는 알림토큰 입니다.", pushToken);
+	}
+}
