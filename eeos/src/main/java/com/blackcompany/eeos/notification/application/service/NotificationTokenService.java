711cc76a7f8074e32ad8768f1086b7c0e7cbb01d
dongE <wlsemrdl1212@naver.com>
2026-01-04 23:32:01 +0900

    chore : spotlessApply

--- eeos/src/main/java/com/blackcompany/eeos/notification/application/service/NotificationTokenService.java
+++ eeos/src/main/java/com/blackcompany/eeos/notification/application/service/NotificationTokenService.java
@@ -11,7 +11,6 @@ import com.blackcompany.eeos.notification.application.usecase.CreateMemberPushTo
 import com.blackcompany.eeos.notification.application.usecase.DeleteAllMemberPushTokensUsecase;
 import com.blackcompany.eeos.notification.application.usecase.DeleteMemberPushTokenUsecase;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
