7c10b1cb61e89549f2b8beb53e2a23488b7550b8
dongE <wlsemrdl1212@naver.com>
2026-01-24 16:41:35 +0900

    remove : 알림 허용, 비허용 네이밍 변경 (pushStatus -> notificationPermission)에 따른 기존 파일 삭제

--- eeos/src/main/java/com/blackcompany/eeos/notification/application/usecase/UpdateNotificationStatusUsecase.java
+++
@@ -1,7 +0,0 @@
-package com.blackcompany.eeos.notification.application.usecase;
-
-import com.blackcompany.eeos.notification.application.dto.UpdatePushStatusRequest;
-
-public interface UpdateNotificationStatusUsecase {
-	void updateStatus(Long memberId, UpdatePushStatusRequest request);
-}
