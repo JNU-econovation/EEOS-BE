65cb4baaa987d4993296ec2991d11db6a8ee37ea
dongE <wlsemrdl1212@naver.com>
2026-01-26 21:04:36 +0900

    feat : 알림 허용 상태 변경에 대한 요청 dto

---
+++ eeos/src/main/java/com/blackcompany/eeos/notification/application/dto/UpdateNotificationPermissionRequest.java
@@ -0,0 +1,18 @@
+package com.blackcompany.eeos.notification.application.dto;
+
+import com.blackcompany.eeos.notification.application.model.NotificationPermission;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class UpdateNotificationPermissionRequest {
+
+	@NotBlank(message = "푸시 토큰은 필수입니다.")
+	private String pushToken;
+	@NotNull(message = "푸시 상태값은 필수입니다.")
+	private NotificationPermission notificationPermission;
+}
