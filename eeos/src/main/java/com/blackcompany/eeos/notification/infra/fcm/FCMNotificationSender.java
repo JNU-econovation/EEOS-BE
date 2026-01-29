a1705fb98b5cd3c5e4eb070c42295101465bcfdd
dongE <wlsemrdl1212@naver.com>
2026-01-29 21:14:35 +0900

    feat : fcm 일괄전송 multicast 메서드 추가

--- eeos/src/main/java/com/blackcompany/eeos/notification/infra/fcm/FCMNotificationSender.java
+++ eeos/src/main/java/com/blackcompany/eeos/notification/infra/fcm/FCMNotificationSender.java
@@ -1,15 +1,22 @@
 package com.blackcompany.eeos.notification.infra.fcm;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Component;
 
 import com.blackcompany.eeos.notification.application.port.NotificationErrorCode;
 import com.blackcompany.eeos.notification.application.port.NotificationMessage;
 import com.blackcompany.eeos.notification.application.port.NotificationResult;
 import com.blackcompany.eeos.notification.application.port.NotificationSender;
+import com.google.firebase.messaging.BatchResponse;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.MulticastMessage;
 import com.google.firebase.messaging.Notification;
+import com.google.firebase.messaging.SendResponse;
 
 import lombok.RequiredArgsConstructor;
 
@@ -32,7 +39,6 @@ public class FCMNotificationSender implements NotificationSender {
 			)
 			.build();
 
-
 		try{
 			firebaseMessaging.send(message);
 			return NotificationResult.success();
@@ -40,5 +46,47 @@ public class FCMNotificationSender implements NotificationSender {
 			NotificationErrorCode errorCode = fcmErrorMapper.map(e);
 			return NotificationResult.fail(errorCode);
 		}
+	}
+
+	@Override
+	public Map<String, NotificationResult> sendAll(String title, String body, List<String> tokens) {
+		MulticastMessage message = MulticastMessage.builder()
+			.setNotification(Notification.builder()
+				.setTitle(title)
+				.setBody(body)
+				.build())
+			.addAllTokens(tokens)
+			.build();
+
+		Map<String, NotificationResult> results = new HashMap<>();
+		try{
+			BatchResponse response = firebaseMessaging.sendEachForMulticast(message);
+
+			if(response.getFailureCount() == 0){
+				for(String token : tokens){
+					results.put(token, NotificationResult.success());
+				}
+				return results;
+			}
+
+			List<SendResponse> sendResponses = response.getResponses();
+			for(int i =0; i < sendResponses.size(); i++){
+				String token = tokens.get(i);
+				SendResponse sendResponse = sendResponses.get(i);
+
+				if(sendResponse.isSuccessful()){
+					results.put(token, NotificationResult.success());
+				}else{
+					NotificationErrorCode errorCode = fcmErrorMapper.map(sendResponse.getException());
+					results.put(token, NotificationResult.fail(errorCode));
+				}
+			}
+		} catch (FirebaseMessagingException e) {
+			for(String token : tokens){
+				results.put(token, NotificationResult.fail(NotificationErrorCode.TEMPORARY_ERROR));
+			}
+		}
+
+		return results;
 	}
 }
