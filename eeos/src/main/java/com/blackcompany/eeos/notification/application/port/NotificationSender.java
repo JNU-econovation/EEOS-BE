f15c51f26ab2d6b6f11f2ce75fbab58085c0531e
dongE <wlsemrdl1212@naver.com>
2026-01-29 21:15:00 +0900

    feat : sendAll 메서드 추상화

--- eeos/src/main/java/com/blackcompany/eeos/notification/application/port/NotificationSender.java
+++ eeos/src/main/java/com/blackcompany/eeos/notification/application/port/NotificationSender.java
@@ -1,5 +1,10 @@
 package com.blackcompany.eeos.notification.application.port;
 
+import java.util.List;
+import java.util.Map;
+
 public interface NotificationSender {
 	NotificationResult send(NotificationMessage notificationMessage);
+
+	Map<String, NotificationResult> sendAll(String title, String body, List<String> tokens);
 }
