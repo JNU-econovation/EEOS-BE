3efb2f8357b9c27524d266a5b8b684412fc2aee7
dongE <wlsemrdl1212@naver.com>
2026-02-10 16:45:44 +0900

    refactor : @EnableRetry 추가

--- eeos/src/main/java/com/blackcompany/eeos/config/RetryConfig.java
+++ eeos/src/main/java/com/blackcompany/eeos/config/RetryConfig.java
@@ -2,11 +2,13 @@ package com.blackcompany.eeos.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.retry.backoff.ExponentialBackOffPolicy;
 import org.springframework.retry.policy.SimpleRetryPolicy;
 import org.springframework.retry.support.RetryTemplate;
 
 @Configuration
+@EnableRetry
 public class RetryConfig {
 
 	@Bean
