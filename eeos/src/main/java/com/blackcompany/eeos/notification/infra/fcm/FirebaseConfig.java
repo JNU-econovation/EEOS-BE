d8ee396f998c4ac16faad444e29e74c33c37509a
dongE <wlsemrdl1212@naver.com>
2026-02-10 19:51:43 +0900

    chore : spotlessApply

--- eeos/src/main/java/com/blackcompany/eeos/notification/infra/fcm/FirebaseConfig.java
+++ eeos/src/main/java/com/blackcompany/eeos/notification/infra/fcm/FirebaseConfig.java
@@ -10,7 +10,6 @@ import java.util.Base64;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class FirebaseConfig {
