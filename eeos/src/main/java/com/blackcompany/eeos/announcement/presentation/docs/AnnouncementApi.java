20add72bff1b5a127447b2118ea57e96ce76481c
Bellmin <whdals011203@naver.com>
2026-03-15 22:13:33 +0900

    docs: AnnouncementApi, AnnouncementInboundApi Swagger 어노테이션 추가

--- eeos/src/main/java/com/blackcompany/eeos/announcement/presentation/docs/AnnouncementApi.java
+++ eeos/src/main/java/com/blackcompany/eeos/announcement/presentation/docs/AnnouncementApi.java
@@ -3,8 +3,12 @@ package com.blackcompany.eeos.announcement.presentation.docs;
 import com.blackcompany.eeos.announcement.application.dto.GetAnnouncementsResponse;
 import com.blackcompany.eeos.common.presentation.response.ApiResponse;
 import com.blackcompany.eeos.common.presentation.response.ApiResponseBody.SuccessBody;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
+@Tag(name = "공지사항", description = "공지사항 조회 API")
 public interface AnnouncementApi {
 
+	@Operation(summary = "공지사항 목록 조회", description = "저장된 공지사항 목록을 최신순으로 조회합니다.")
 	ApiResponse<SuccessBody<GetAnnouncementsResponse>> getAnnouncements();
 }
--- eeos/src/main/java/com/blackcompany/eeos/announcement/presentation/docs/AnnouncementInboundApi.java
+++ eeos/src/main/java/com/blackcompany/eeos/announcement/presentation/docs/AnnouncementInboundApi.java
@@ -3,8 +3,14 @@ package com.blackcompany.eeos.announcement.presentation.docs;
 import com.blackcompany.eeos.announcement.application.dto.SaveAnnouncementRequest;
 import com.blackcompany.eeos.common.presentation.response.ApiResponse;
 import com.blackcompany.eeos.common.presentation.response.ApiResponseBody.SuccessBody;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
+@Tag(name = "공지사항 내부 API", description = "Slack 메시지를 수신하여 공지사항으로 저장하는 내부 API")
 public interface AnnouncementInboundApi {
 
+	@Operation(
+			summary = "Slack 공지 메시지 수신",
+			description = "Slack 공지 채널 메시지를 수신하여 공지사항으로 저장합니다. X-EEOS-API-KEY 헤더 인증이 필요합니다.")
 	ApiResponse<SuccessBody<Void>> save(SaveAnnouncementRequest request);
 }
