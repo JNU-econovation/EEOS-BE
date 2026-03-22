f55c8092d9edda04c8f3a87ecb8dafed5500127e
mando <mando@krafton.com>
2026-03-22 23:32:27 +0900

    feat: update cookie config - SameSite Lax, separate AT/RT paths

--- eeos/src/main/java/com/blackcompany/eeos/auth/presentation/support/AuthConstants.java
+++ eeos/src/main/java/com/blackcompany/eeos/auth/presentation/support/AuthConstants.java
@@ -2,4 +2,6 @@ package com.blackcompany.eeos.auth.presentation.support;
 
 public class AuthConstants {
 	public static final String TOKEN_KEY = "eeos_token";
+	public static final String ACCESS_TOKEN_KEY = "eeos_access_token";
+	public static final String REFRESH_TOKEN_KEY = "eeos_refresh_token";
 }
