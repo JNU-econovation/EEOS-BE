b08bc385f25ef2fc7e3c3ae8fbf3617caf0cabd0
Bellmin <whdals011203@naver.com>
2026-03-28 20:24:11 +0900

    feat: Gemini API 예외 클래스 추가

---
+++ eeos/src/main/java/com/blackcompany/eeos/announcement/application/support/GeminiApiException.java
@@ -0,0 +1,8 @@
+package com.blackcompany.eeos.announcement.application.support;
+
+public class GeminiApiException extends RuntimeException {
+
+	public GeminiApiException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
