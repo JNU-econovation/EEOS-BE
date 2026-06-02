0a036306769e41a1eb5a638d72fa3e7672eba817
kmando01 <157668827+kmando01@users.noreply.github.com>
2026-06-02 14:40:06 +0900

    fix: SecurityConfig에서 AccessTokenFilter 빈 등록 제거

--- eeos/src/main/java/com/blackcompany/eeos/config/security/SecurityConfig.java
+++ eeos/src/main/java/com/blackcompany/eeos/config/security/SecurityConfig.java
@@ -8,14 +8,6 @@ import org.springframework.context.annotation.Configuration;
 public class SecurityConfig {
 
 	@Bean
-	public FilterRegistrationBean<AccessTokenFilter> auth(AccessTokenFilter authFilter) {
-		FilterRegistrationBean<AccessTokenFilter> registrationBean = new FilterRegistrationBean<>();
-		registrationBean.setFilter(authFilter);
-		registrationBean.setEnabled(false);
-		return registrationBean;
-	}
-
-	@Bean
 	public FilterRegistrationBean<OptionsFilter> optionsFilterRegistrationBean(
 			OptionsFilter optionsFilter) {
 		FilterRegistrationBean<OptionsFilter> registrationBean =
